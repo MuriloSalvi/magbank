7188c05f3fa9612ce5dbdfd38b2dd7416992792f
Murilo D' Salvi Furlan <56124167+MuriloSalvi@users.noreply.github.com>
2021-06-28 19:18:19 -0300

    pagina inicial caminhando

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero'
 import CreditCard from './components/CreditCard'
+import CardList from './components/CardList';
+import CenteredButton from './components/CenteredButon';
+import posts from './data/posts'
+
 import './App.scss';
 
 const App = ()=>{
@@ -10,6 +14,8 @@ const App = ()=>{
       <Navbar/>
       <Hero/>
       <CreditCard/>
+      <CardList posts={posts}/>
+      <CenteredButton><b>Abra sua conta</b></CenteredButton>
     </div>
     
   );
